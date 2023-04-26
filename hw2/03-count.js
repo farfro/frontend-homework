d91ced2c9cac7d9d0a0f5e5c47f7e333b973377a
farfro <samideacon17@gmail.com>
2023-04-26 15:40:01 -0700

    completed hw2

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,16 @@
 // Add your code here
 
-input.addEventListener('keydown', handleKeyDown);
+//https://stackoverflow.com/questions/74904016/how-to-highlight-all-matching-words-in-a-paragraph-when-searching-multiple-words
+
+let input = document.querySelector('#word');
+let para = document.querySelector('#find');
+
+input.addEventListener('keydown', (event) => {
+    if(event.key === 'Enter') {
+        let word = new RegExp('\\b' + input.value + '\\b', "g");
+        let find = para.textContent;
+        para.innerHTML = find.replace(word, `<span style="background-color: yellow;">${input.value}</span>`);
+    }
+});
+
+
--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,24 @@
 // Add your code here
+const fullname = document.querySelector("#fullname");
+const email = document.querySelector("#email");
+const registration = document.querySelector("#selection");
+const programming = document.querySelector("#check1");
+const operating = document.querySelector("#check2");
+const devlopement = document.querySelector("#check3");
+const extra = document.querySelector("#extra");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    console.log(`Full Name: ${fullname.value}`);
+    console.log(`Email: ${email.value}`);
+    console.log(`Registration Status: ${registration.value}`);
+    console.log(`Programming Languages: ${programming.checked}`);
+    console.log(`Operating Systems: ${operating.checked}`);
+    console.log(`Full Stack Web Development: ${devlopement.checked}`);
+    console.log(`anything else: ${extra.value}`);
+
+    form.reset();
+  });
